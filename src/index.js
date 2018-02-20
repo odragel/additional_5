b1d8fecb91129396acad4e2f5c6971b3b17be68f
odragel <olga.dragel@gmail.com>
2018-02-20 22:57:28 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var openedBrackets=[];
+    for (var i = 0; i < str.length; i++) {
+        var temp = str.charAt(i);
+        var isOpened = 0;
+        debugger;
+        for (var j = 0; j < bracketsConfig.length; j++){
+            if (temp == bracketsConfig[j][0]){
+
+                isOpened = 1;
+                if(bracketsConfig[j][0] == bracketsConfig[j][1]){
+                    if(openedBrackets.length > 0){
+                        if(openedBrackets[openedBrackets.length-1]!= temp){
+                            openedBrackets.push(temp);
+                        }else{
+                            openedBrackets.pop();
+                        }
+                    }
+                }
+                else{
+                    openedBrackets.push(temp);
+                }
+                break;
+            }
+        }
+        if(!isOpened){
+            for (var j = 0; j < bracketsConfig.length; j++){
+                if (temp == bracketsConfig[j][1]){
+                    if (openedBrackets[openedBrackets.length-1] == bracketsConfig[j][0]){
+                        openedBrackets.pop();
+                        break;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+
+    }
+    if (openedBrackets.length == 0){
+        return true;
+    }else {
+        return false;
+    }
 }
